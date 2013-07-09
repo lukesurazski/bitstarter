6b80b1652d5dfd7f5f51a0c93e1e9544f3cea4f6
Luke Surazski <lsurazsk@gmail.com>
2013-07-09 21:59:14 +0000

    Added readfilesync to web.js

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var output_buf = fs.readFile("index.html");
+  var output_buf = fs.readFileSync("index.html");
   var output_str = output_buf.toString();
   response.send(output_str);
 });
